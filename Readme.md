aad9ecd1a694c4106999fa2ca7de2b151c556f54
sikari <ashish.adhikari@bison.howard.edu>
2018-02-21 22:50:42 -0500

    working on merge_sort

--- Readme.md
+++ Readme.md
@@ -1,4 +1,4 @@
 ###Assumptions:
 - [X] Integers in **data.txt** are separated by newline ```\n``` character
 - [X] The last line does not have a newline character 
-- [X] No empty lines exist
+- [X] No empty line exists
--- sort.c
+++ sort.c
@@ -1,23 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h> 
+
+#define NUM_THREADS 2
+
+//to keep track of index of subarrays
+typedef struct tracker{
+	int lower_index;
+	int higher_index;
+} tracker;
+
+//keep track of elements in the array
+int count = 0;
+int *list;
+
+
+void *merge_sort(void *boundary){
+	
+}
+
+
+
 int main(int argc, char** argv)
 {
     FILE *fp;
     fp = fopen(argv[1], "r");
     if(!fp) return 1;
 
-    //get the number of integers in file before allocating dynamic memory
-    int count = 0;
+    //the number of integers in file
     for (char c = getc(fp); c != EOF; c = getc(fp))
         if (c == '\n') // Increment count if this character is newline
-            count = count + 1;
+            count+=1;
     
-    //the list of numbers
-    int* list = (int*)malloc(sizeof(int)*(count+1));
-    //reset the filepointer
+     //reset filepointer to populate all integers
     fseek(fp, 0, SEEK_SET);
-
-    char temp = '\n';
+    list = (int*)malloc(sizeof(int)*(count++));
     int i = 0;
     //populate array with index 0 ->= count
     while(!feof(fp))
@@ -25,8 +41,14 @@ int main(int argc, char** argv)
     	fscanf(fp,"%d",&list[i]);
     	i++;
     }
+    //initialise indices 
+	tracker root;
+	root.lower_index = 0;
+	root.higher_index = count-1;
+	printf("%p\n", list);
+	pthread_t root_thread;
 
-    
+	pthread_create(&thread,NULL,merge_sort);
     
     return 0;
 }
